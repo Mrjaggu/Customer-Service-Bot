2ecd962e789a4415e402e90bb607154a0451d894
Ajay Jangid <40687103+Mrjaggu@users.noreply.github.com>
2020-09-02 11:35:38 +0530

    Delete start_services.sh

--- start_services.sh
+++
@@ -1,7 +0,0 @@
-# Start rasa server with nlu model
-
-rasa run -m models --credentials credentials.yml --enable-api --log-file out.log --endpoints endpoints.yml
-
-rasa run actions
-
-python3 app.py
